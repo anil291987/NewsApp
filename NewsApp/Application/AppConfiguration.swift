3d610f9ed8d473195eaa0d026aeb4cd00b1ec321
Anil <markadam714@gmail.com>
2024-12-11 21:02:45 +0530

    Update AppConfiguration.swift

--- NewsApp/Application/AppConfiguration.swift
+++ NewsApp/Application/AppConfiguration.swift
@@ -7,7 +7,7 @@ import Foundation
 
 struct AppConfiguration {
     static let baseURL = "https://newsapi.org/v2"
-    static let apiKey = "8a5f2fed933340d2ab84f6383152ecf1"
+    static let apiKey = "apikey"
     static let everyThings = "everything"
     static let topHeadlines = "top-headlines"
 }
